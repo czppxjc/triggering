358dd928e38f5304ebb72233a56d0157f016e8a5
nadinebade <54040089+nadinebade@users.noreply.github.com>
2020-10-06 16:49:31 +0200

    added new rotation and new css

--- materials2.js
+++ materials2.js
@@ -13,9 +13,10 @@ var teach = [
   + "<div class='box6'></div>"
   + "<div class='box7'></div>"
   + "<div class='box8'></div>"
-  + "<div class='square45'></div>",
+  + "<div class='box9'></div>"
+  + "<div class='square95'></div>",
   id: "teaching.1",
-  nickname: "square45",
+  nickname: "square95",
   shape: "square"
   },
 
@@ -28,9 +29,10 @@ var teach = [
   + "<div class='box6'></div>"
   + "<div class='box7'></div>"
   + "<div class='box8'></div>"
-  + "<div class='rectangle45'></div>",
+  + "<div class='box9'></div>"
+  + "<div class='rectangle95'></div>",
   id: "teaching.2",
-  nickname: "rectangle45",
+  nickname: "rectangle95",
   shape: "rectangle"
   },
 
@@ -42,9 +44,10 @@ var teach = [
   + "<div class='box6'></div>"
   + "<div class='box7'></div>"
   + "<div class='box8'></div>"
-  + "<div class='dot45'></div>",
+  + "<div class='box9'></div>"
+  + "<div class='dot95'></div>",
   id: "teaching.3",
-  nickname: "circle45",
+  nickname: "circle95",
   shape: "circle"
   },
 
@@ -62,6 +65,22 @@ var test = [
   + "<div class='box6'></div>"
   + "<div class='box7'></div>"
   + "<div class='box8'></div>"
+  + "<div class='box9'></div>"
+  + "<div class='square95'></div>",
+  id: "testing.1",
+  nickname: "square95",
+  shape: "square"
+  },
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='box5'></div>"
+  + "<div class='box6'></div>"
+  + "<div class='box7'></div>"
+  + "<div class='box8'></div>"
+  + "<div class='box9'></div>"
   + "<div class='square45'></div>",
   id: "testing.1",
   nickname: "square45",
@@ -76,6 +95,7 @@ var test = [
   + "<div class='box6'></div>"
   + "<div class='box7'></div>"
   + "<div class='box8'></div>"
+  + "<div class='box9'></div>"
   + "<div class='square23'></div>",
   id: "testing.2",
   nickname: "square23",
@@ -90,6 +110,7 @@ var test = [
   + "<div class='box6'></div>"
   + "<div class='box7'></div>"
   + "<div class='box8'></div>"
+  + "<div class='box9'></div>"
   + "<div class='square54'></div>",
   id: "testing.3",
   nickname: "square54",
@@ -104,6 +125,7 @@ var test = [
   + "<div class='box6'></div>"
   + "<div class='box7'></div>"
   + "<div class='box8'></div>"
+  + "<div class='box9'></div>"
   + "<div class='square27'></div>",
   id: "testing.4",
   nickname: "square27",
