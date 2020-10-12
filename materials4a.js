609a96f6ab166be4b3e38adbe949dae6bc1d5dba
nbade <nadine.bade@ens.fr>
2020-10-12 11:29:11 +0200

    adding simpler version of grid

---
+++ materials4a.js
@@ -0,0 +1,131 @@
+// this is an array with all of the materials to be presented, order doesn't
+// matter since this will be randomized.  each item needs a unique ID
+
+
+var teach = [
+
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='square12'></div>",
+  id: "teaching.1",
+  nickname: "square12",
+  shape: "square"
+  },
+
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='rectangle12'></div>",
+  id: "teaching.2",
+  nickname: "rectangle12",
+  shape: "rectangle"
+  },
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='dot12'></div>",
+  id: "teaching.3",
+  nickname: "circle12",
+  shape: "circle"
+  },
+
+
+];
+
+
+var test = [
+
+
+    { subject: "<div class='box1'></div>"
+    + "<div class='box2'></div>"
+    + "<div class='box3'></div>"
+    + "<div class='box4'></div>"
+    + "<div class='square12'></div>",
+    id: "testing.1",
+    nickname: "sq12",
+    shape: "square"
+    },
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='square21'></div>",
+  id: "testing.2",
+  nickname: "sq21",
+  shape: "square"
+  },
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='square34'></div>",
+  id: "testing.3",
+  nickname: "sq34",
+  shape: "square"
+  },
+
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='square43'></div>",
+  id: "testing.4",
+  nickname: "sq43",
+  shape: "square"
+  },
+
+
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='square24'></div>",
+  id: "testing.5",
+  nickname: "sq24",
+  shape: "square"
+  },
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='square42'></div>",
+  id: "testing.6",
+  nickname: "sq42",
+  shape: "square"
+  },
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='square13'></div>",
+  id: "testing.7",
+  nickname: "sq13",
+  shape: "square"
+  },
+
+  { subject: "<div class='box1'></div>"
+  + "<div class='box2'></div>"
+  + "<div class='box3'></div>"
+  + "<div class='box4'></div>"
+  + "<div class='square31'></div>",
+  id: "testing.8",
+  nickname: "sq31",
+  shape: "square"
+  },
+
+
+
+];
