047eb298a1db7a971bc9a2676021b3dc8f3f5d32
nadinebade <54040089+nadinebade@users.noreply.github.com>
2020-12-08 15:02:09 +0100

    add new results and analysis

---
+++ exp2/results/exp2_analysis.R
@@ -0,0 +1,72 @@
+# This script loads the data
+
+### LOAD PACKAGES
+library(dplyr)
+library(plyr)
+library(ggplot2)
+library(stringr)
+library(lsr)
+library(lme4)
+#library(lmTest)
+library(lmerTest)
+library(simr)
+library(emmeans)
+library(exactRankTests)
+library(pwr)
+library(psych)
+library(boot)
+library(rcompanion)
+library(rsq)
+library(tidyr)
+library(naniar)
+library(RColorBrewer)
+
+
+
+
+data = read.csv(file=file.choose(),sep = ',',header = T,na.strings=c("","NA"))
+
+
+length(unique(data$subjectId))
+
+#40
+
+
+head(data)
+
+
+exp2 <- subset(data, type == "testing")
+
+is.numeric(exp2$button_pressed)
+
+levels(exp2$condition)
+
+exp2$negation <- ifelse(exp2$condition == "ABneg" | exp2$condition == "AnotB_greyneg" | exp2$condition
+                        == "AnotB_redneg" | exp2$condition == "notABneg" | exp2$condition == "notAnotBneg", "did not wug", "wugged")
+
+exp2$cond <- ifelse(exp2$condition == "AB" | exp2$condition == "ABneg", "AB",
+                    ifelse(exp2$condition == "AnotB_grey" | exp2$condition == "AnotB_greyneg"
+                           | exp2$condition == "AnotB_red" | exp2$condition == "AnotB_redneg", "AnotB",
+            ifelse(exp2$condition == "notAB" | exp2$condition == "notABneg", "notAB", "notAnotB")))
+
+results <- ddply(exp2, .(cond, group, negation), summarize, true = mean(button_pressed, na.rm =TRUE),
+                 false = 1-mean(button_pressed, na.rm = TRUE))
+
+
+
+### plotting
+
+
+plot <- ggplot(data=results, aes(x=cond, y=true, fill = negation)) +
+  geom_bar(stat="identity", position=position_dodge())+
+  facet_wrap(~group)
+#+geom_errorbar(aes(ymin=M-SE, ymax=M+SE), width=.2,
+ #               position=position_dodge(.9))+
+#  theme_classic(base_size = 20) 
+
+plot +   labs(title="",
+                           x="", y = "%yes-responses")
+
+
+
+
