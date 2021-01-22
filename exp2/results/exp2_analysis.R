90e9b52c8d17ad6bd93d4f7ec3423afeb1dc01d7
nadinebade <54040089+nadinebade@users.noreply.github.com>
2021-01-22 09:34:57 +0100

    results of pilot 2

--- exp2/results/exp2_analysis.R
+++ exp2/results/exp2_analysis.R
@@ -53,6 +53,15 @@ results <- ddply(exp2, .(cond, group, negation), summarize, true = mean(button_p
                  false = 1-mean(button_pressed, na.rm = TRUE))
 
 
+exp2$cond2 <- ifelse(exp2$condition == "AB" | exp2$condition == "ABneg", "AB",
+                    ifelse(exp2$condition == "AnotB_grey" | exp2$condition == "AnotB_greyneg", "AnotB_grey",
+                           ifelse(exp2$condition == "AnotB_red" | exp2$condition == "AnotB_redneg", "AnotB_red",
+                           ifelse(exp2$condition == "notAB" | exp2$condition == "notABneg", "notAB", "notAnotB"))))
+
+                    
+results2 <- ddply(exp2, .(cond2, group, negation), summarize, true = mean(button_pressed, na.rm =TRUE),
+                                     false = 1-mean(button_pressed, na.rm = TRUE))
+
 
 ### plotting
 
@@ -68,5 +77,13 @@ plot +   labs(title="",
                            x="", y = "%yes-responses")
 
 
+plot2 <- ggplot(data=results2, aes(x=cond2, y=true, fill = negation)) +
+  geom_bar(stat="identity", position=position_dodge())+
+  facet_wrap(~group)
+#+geom_errorbar(aes(ymin=M-SE, ymax=M+SE), width=.2,
+#               position=position_dodge(.9))+
+#  theme_classic(base_size = 20) 
 
+plot2 +   labs(title="",
+              x="", y = "%yes-responses")
 
