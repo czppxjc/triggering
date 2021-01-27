2ea787b5662ae47fa600f4dcc345ee9f8333c18f
nadine <nadinebade85@gmail.com>
2021-01-27 16:49:40 +0100

    do not send data to server yet

--- exp3/script.js
+++ exp3/script.js
@@ -195,9 +195,9 @@ timeline.push(demographics_block);
 jsPsych.init({
   timeline: timeline,
   show_progress_bar: true,
-  on_finish: function(data){ SaveData("triggering2",
-                                      theSubject,
-                                      jsPsych.data.get().csv);
-                              $(".jspsych-content").html("<center><p>Thank you for completing the experiment.  <strong>Please enter the code below on Prolific.</strong></p></center></p></center><center><p> 15039F44 </p></center>"); }
-//  on_finish: function(data){ jsPsych.data.displayData("json"); }
+//  on_finish: function(data){ SaveData("triggering2",
+  //                                    theSubject,
+    //                                  jsPsych.data.get().csv);
+      //                        $(".jspsych-content").html("<center><p>Thank you for completing the experiment.  <strong>Please enter the code below on Prolific.</strong></p></center></p></center><center><p> 15039F44 </p></center>"); }
+  on_finish: function(data){ jsPsych.data.displayData("json"); }
 });
