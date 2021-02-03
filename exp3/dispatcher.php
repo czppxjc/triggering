114ad054ed437ce8d78285e2933e08089f190984
nbade <nadinebade8@gmail.com>
2021-02-03 16:15:10 +0100

    updating dispatcher

--- exp3/dispatcher.php
+++ exp3/dispatcher.php
@@ -5,10 +5,9 @@ $counter_val = file_get_contents($counter_file);
 file_put_contents($counter_file, $counter_val + 1);
 
 // Possible redirections
-$redirects = array("https://psycholinguae.fr/nbade/Triggering/exp2/triggering-control.html",
-                  "https://psycholinguae.fr/nbade/Triggering/exp2/triggering-i.html",
-                   "https://psycholinguae.fr/nbade/Triggering/exp2/triggering-ii.html",
-                   "https://psycholinguae.fr/nbade/Triggering/exp2/triggering-iii.html");
+$redirects = array("https://psycholinguae.fr/nbade/triggering/exp3/triggering-i.html",
+                   "https://psycholinguae.fr/nbade/triggering/exp3/triggering-ii.html",
+                   "https://psycholinguae.fr/nbade/triggering/exp3/triggering-iii.html");
 // Redirect user to the next link
 header("Location: " . $redirects[$counter_val % count($redirects)]);
 ?>
