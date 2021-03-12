20e979e0ba6e786515333d87d85c8af06bdc8a37
nbade <nadinebade8@gmail.com>
2021-03-12 15:21:54 +0100

    analysis and data exp4

--- exp4_CB/Analysis/exp4_analysis.R
+++ exp4_CB/Analysis/exp4_analysis.R
@@ -42,94 +42,99 @@ is.numeric(exp2$button_pressed)
 levels(exp2$condition)
 levels(exp2$phrase)
 
-exp2$negation <- ifelse(exp2$phrase == "does not wug.", "negation", "no negation")
+exp2$negation <- ifelse(exp2$phrase == "does not wug.", "negative", "positive")
 
-#exp2$cond <- ifelse((exp2$group == "control" | exp2$group == "wug=direction" | exp2$group == "wug=red") & exp2$condition == "AB", "upward-from red", 
-       #             ifelse(((exp2$group == "control" | exp2$group == "wug=direction") & exp2$condition == "notAB") | (exp2$group== "wug=red" & exp2$condition == "AnotB"), "not upward-from red", 
-        #                   ifelse(((exp2$group == "control" | exp2$group == "wug=direction") & exp2$condition == "AnotB") | (exp2$group== "wug=red" & exp2$condition == "notAB"), "upward-not from red",
-         #                         ifelse((exp2$group == "control" | exp2$group == "wug=direction" | exp2$group == "wug=red") & exp2$condition == "notAnotB", "not upward-not from red", "blubb"))))
+exp2$cond <- ifelse(exp2$condition == "AB", "upward-from red", 
+                  ifelse(exp2$condition == "notAB", "not upward-from red", 
+                          ifelse(exp2$condition == "AnotB", "upward-not from red",
+                              ifelse(exp2$condition == "notAnotB", "not upward-not from red", "blubb"))))
 
-exp2$type <- ifelse(exp2$condition == "AB" | exp2$condition == "AnotB"| exp2$condition == "notAnotB"
-                           | exp2$condition == "notAB", "simple", "quantificational")
+exp2$type <- ifelse(exp2$condition == "AB" | exp2$condition == "AnotB" | exp2$condition == "notAnotB" | exp2$condition == "notAB", "simple", "quantificational")
 
-exp2$index <- ifelse(((exp2$condition == "AnotB" | exp2$condition == "notAnotB" | exp2$condition == "notAB") & exp2$negation == "negation" & exp2$button_pressed == 1) 
-                     | (exp2$condition == "AB" & exp2$negation == "no negation" & exp2$button_pressed == 1)
-| ((exp2$condition == "AnotB" | exp2$condition == "notAnotB" | exp2$condition == "notAB") & exp2$negation == "no negation" & exp2$button_pressed == 0)
-| (exp2$condition == "AB" & exp2$negation == "negation" & exp2$button_pressed == 0), 1, 0)
-
-exp2$index2 <- ifelse(((exp2$cond == "upward-from red" & exp2$button_pressed == 1) | (exp2$cond == "not upward-from red" & exp2$button_pressed == 0)
-| (exp2$cond == "upward-not from red" & exp2$button_pressed == 0) | (exp2$cond == "not upward-not from red" & exp2$button_pressed == 0) & exp2$negation == "no negation") |
-  ((exp2$cond == "upward-from red" & exp2$button_pressed == 0) | (exp2$cond == "not upward-from red" & exp2$button_pressed == 1)
-   | (exp2$cond == "upward-not from red" & exp2$button_pressed == 1) | (exp2$cond == "not upward-not from red" & exp2$button_pressed == 1) & exp2$negation == "negation"), 1, 0)
+exp2$vis_pic <- ifelse(exp2$button_pressed == 0, 1, 0)
 
 
+is.numeric(exp2$CB)
 
-is.numeric(exp2$index2)
+exp3 <- subset(exp2, rt< 9000 & rt> 500)
 
-exp_qua <- subset(exp2, type == "quantificational")
-exp <- subset(exp2, type == "simple")
+boxplot(exp3$rt)
+
+exp_qua <- subset(exp3, type == "quantificational")
+exp <- subset(exp3, type == "simple")
 
 
 
-results1 <- ddply(exp, .(condition, group, negation), summarize, M = mean(index, na.rm =TRUE))
-results1b <- ddply(exp, .(condition, group, negation), summarize, M = mean(button_pressed, na.rm =TRUE))
-results1c <- ddply(exp, .(cond, group, negation), summarize, M = mean(button_pressed == 1, na.rm =TRUE))
+results <- ddply(exp, .(cond, negation), summarize, M = mean(button_pressed, na.rm =TRUE), RT = mean(rt, na.rm =TRUE) )
 
-controls <- subset(exp, group == "control")
-                    
-results2 <- ddply(exp_qua, .(condition, group, negation), summarize, M = mean(button_pressed, na.rm =TRUE))
+results_qua <- ddply(exp_qua, .(condition, negation), summarize, M = mean(button_pressed, na.rm =TRUE), RT = mean(rt, na.rm =TRUE))
 
 
 ### plotting
 
 
-plot <- ggplot(data=results1, aes(x=condition, y=M, fill = negation)) +
-  geom_bar(stat="identity", position=position_dodge())+
-  facet_wrap(~group)
+plot <- ggplot(data=results, aes(x=cond, y=M, fill = negation)) +
+  geom_bar(stat="identity", position=position_dodge())
 #+geom_errorbar(aes(ymin=M-SE, ymax=M+SE), width=.2,
  #               position=position_dodge(.9))+
 #  theme_classic(base_size = 20) 
 
 plot +   labs(title="",
-                           x="", y = "PSP index")
+                           x="", y = "rate of CB choices")
 
-plot <- ggplot(data=results1c, aes(x=group, y=M, fill = negation)) +
-  geom_bar(stat="identity", position="stack")+
-  facet_wrap(~cond)
-#+geom_errorbar(aes(ymin=M-SE, ymax=M+SE), width=.2,
-#               position=position_dodge(.9))+
-#  theme_classic(base_size = 20) 
-
-plot +   labs(title="",
-              x="", y = "yes-responses")
-
-
-plot2 <- ggplot(data=results2, aes(x=condition, y=M, fill=group)) +
+plot_rt <- ggplot(data=results, aes(x=cond, y=RT, fill = negation)) +
   geom_bar(stat="identity", position=position_dodge())
 #+geom_errorbar(aes(ymin=M-SE, ymax=M+SE), width=.2,
 #               position=position_dodge(.9))+
 #  theme_classic(base_size = 20) 
 
-plot2 +   labs(title="",
-              x="", y = "PSP index")
+plot_rt +   labs(title="",
+              x="", y = "RTs")
 
 
 
 
-exp %>% ddply(.(condition, group, negation, subjectId), 
+
+
+plot <- ggplot(data=results_qua, aes(x=condition, y=M)) +
+  geom_bar(stat="identity", position=position_dodge())
+#+geom_errorbar(aes(ymin=M-SE, ymax=M+SE), width=.2,
+#               position=position_dodge(.9))+
+#  theme_classic(base_size = 20) 
+
+plot +   labs(title="",
+              x="", y = "rate of CB choices")
+
+
+
+plot_rt_qua <- ggplot(data=results_qua, aes(x=condition, y=RT)) +
+  geom_bar(stat="identity", position=position_dodge())
+#+geom_errorbar(aes(ymin=M-SE, ymax=M+SE), width=.2,
+#               position=position_dodge(.9))+
+#  theme_classic(base_size = 20) 
+
+plot_rt_qua +   labs(title="",
+              x="", y = "RTs")
+
+
+
+
+
+
+
+exp %>% ddply(.(cond, negation, subjectId), 
 		summarize, 
                m = mean(button_pressed)
 		) %>%
-	ddply(.(condition, group, negation), 
+	ddply(.(cond, negation), 
 		summarize, 
 		       n    = length(!is.na(m)),
                m = mean(m, na.rm=TRUE),
                sd   = sd(m, na.rm=TRUE),
                se   = sd / sqrt(n)
 		) %>%
-	ggplot(data=., aes(x=group, y=m, fill=negation)) +
+	ggplot(data=., aes(x=cond, y=m, fill=negation)) +
 		geom_bar(stat="identity", position="stack") +
-		facet_grid(~condition) +   
 		labs(title="", x="", y = "1=bivalence, less=pres gap")
 
 
