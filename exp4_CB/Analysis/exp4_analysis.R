d59a7b26a4aee16d33c3df8fd482788b98553aa1
nadinebade <54040089+nadinebade@users.noreply.github.com>
2021-05-26 13:54:59 +0200

    analysis revision

--- exp4_CB/Analysis/exp4_analysis.R
+++ exp4_CB/Analysis/exp4_analysis.R
@@ -35,15 +35,19 @@ length(unique(data$subjectId))
 head(data)
 
 
-exp2 <- subset(data, type == "testing")
+exp3 <- subset(data, type == "testing")
 
-is.numeric(exp2$button_pressed)
+is.numeric(exp3$button_pressed)
 
-levels(exp2$condition)
-levels(exp2$phrase)
-levels(exp2$group)
+levels(exp3$condition)
+levels(exp3$phrase)
+levels(exp3$group)
 
-exp2$negation <- ifelse((exp2$phrase == "does not wug." | exp2$phrase == "does not move from red upward.") , "negative", "positive")
+exp3$negation <- ifelse((exp3$phrase == "does not wug." | exp3$phrase == "does not move from red upward.") , "negative", "positive")
+
+exp2 <- subset(exp3, group != "control")
+
+length(unique(exp2$subjectId))
 
 exp2$cond <- ifelse(exp2$condition == "AB", "upward-from red", 
                   ifelse(exp2$condition == "notAB", "not upward-from red", 
@@ -52,16 +56,21 @@ exp2$cond <- ifelse(exp2$condition == "AB", "upward-from red",
 
 exp2$type <- ifelse(exp2$condition == "AB" | exp2$condition == "AnotB" | exp2$condition == "notAnotB" | exp2$condition == "notAB", "simple", "quantificational")
 
+exp2$target <- ifelse(exp2$cond == "upward-from red" | exp2$cond == "not upward-not from red", "baseline", "target")
 
+exp2$critical <- ifelse(exp2$cond == "upward-not from red" | exp2$cond == "not upward-not from red" | exp2$cond == "not upward-from red", "critical", "learned")
+
+exp2$novelty <- ifelse((exp2$group == "wug=red" & (exp2$cond == "upward-from red" | exp2$cond == "upward-not from red")) |
+                         (exp2$group == "wug=upward" & (exp2$cond == "upward-from red" | exp2$cond == "not upward-from red")) | 
+                         (exp2$group == "no training" & exp2$cond == "upward-from red"),"given", "novel")
+                         
+                         
 
 is.numeric(exp2$CB)
 
-exp3 <- subset(exp2, rt< 9000 & rt> 500)
 
-boxplot(exp3$rt)
-
-exp_qua <- subset(exp3, type == "quantificational")
-exp <- subset(exp3, type == "simple")
+exp_qua <- subset(exp2, type == "quantificational")
+exp <- subset(exp2, type == "simple")
 
 
 
@@ -74,25 +83,38 @@ results_pred = read.csv(file=file.choose(),sep = ';',header = T,na.strings=c("",
 ### plotting
 ##write.table(results, file = "results.txt", sep = ",", quote = FALSE, row.names = F)
 
-plot <- ggplot(data=subset(results_pred, group == "control")) +
-  geom_bar(aes(x=exp, y=M, fill = negation), stat="identity", position="dodge", width = 0.8, color = "black")+
+plot <- ggplot(data=subset(results_pred, exp == "predicted")) +
+  geom_bar(aes(x=group, y=M, fill = negation), stat="identity", position="dodge", width = 0.8, color = "black")+
 facet_wrap(~cond)+
 #+geom_errorbar(aes(ymin=M-SE, ymax=M+SE), width=.2,
  #               position=position_dodge(.9))+
   theme_classic(base_size = 20) 
 
-plot +   labs(title="training: no training - controls",
-                           x="", y = "rate of visible picture choices")
+plot +   labs(title="Predictions under H1a and H2a",
+                           x="", y = "predicted rate of visible picture choices")
 
-plot_rt <- ggplot(data=results, aes(x=group, y=RT, fill = negation)) +
+plot_m <- ggplot(data=results, aes(x=group, y=M, fill=negation)) +
+  geom_bar(stat="identity", position=position_dodge())+
+  facet_wrap(~cond)+
+#+geom_errorbar(aes(ymin=M-SE, ymax=M+SE), width=.2,
+#               position=position_dodge(.9))+
+  theme_classic(base_size = 20) 
+
+plot_m +   labs(title="Results by training, animation, polarity",
+              x="training", y = "mean visible picture choices")
+
+results2 <- ddply(exp, .(cond, novelty, negation, group), summarize, M = mean(button_pressed==0, na.rm =TRUE), RT = mean(rt, na.rm =TRUE) )
+
+
+plot_m2 <- ggplot(data=results2, aes(x=group, y=M, fill = novelty)) +
   geom_bar(stat="identity", position=position_dodge())+
   facet_wrap(~cond)
 #+geom_errorbar(aes(ymin=M-SE, ymax=M+SE), width=.2,
 #               position=position_dodge(.9))+
 #  theme_classic(base_size = 20) 
 
-plot_rt +   labs(title="",
-              x="", y = "RTs")
+plot_m2 +   labs(title="",
+                x="", y = "mean visible picture choices")
 
 
 
@@ -124,37 +146,163 @@ plot_rt_qua +   labs(title="",
 
 
 
+#### STATS
 
 
-exp %>% ddply(.(cond, negation, subjectId), 
-		summarize, 
-               m = mean(button_pressed)
-		) %>%
-	ddply(.(cond, negation), 
-		summarize, 
-		       n    = length(!is.na(m)),
-               m = mean(m, na.rm=TRUE),
-               sd   = sd(m, na.rm=TRUE),
-               se   = sd / sqrt(n)
-		) %>%
-	ggplot(data=., aes(x=cond, y=m, fill=negation)) +
-		geom_bar(stat="identity", position="stack") +
-		labs(title="", x="", y = "1=bivalence, less=pres gap")
+model_0 <- glmer(button_pressed ~ novelty*cond + (1+cond| subjectId), 
+                  family = "binomial", 
+                  data =  exp,
+                  control=glmerControl(optimizer="bobyqa",
+                                       optCtrl=list(maxfun=2e5)))
+
+
+summary(model_0)
+
+model_0b <- glmer(button_pressed ~ novelty+cond + (1+cond| subjectId), 
+                 family = "binomial", 
+                 data =  exp,
+                 control=glmerControl(optimizer="bobyqa",
+                                      optCtrl=list(maxfun=2e5)))
+
+
+summary(model_0b)
+
+anova(model_0, model_0b)
+
+
+model_0a <- glmer(button_pressed ~ group*cond + (1+cond| subjectId), 
+                 family = "binomial", 
+                 data =  exp,
+                 control=glmerControl(optimizer="bobyqa",
+                                      optCtrl=list(maxfun=2e5)))
+
+
+summary(model_0a)
+
+model_0c <- glmer(button_pressed ~ group+cond + (1+cond| subjectId), 
+                  family = "binomial", 
+                  data =  exp,
+                  control=glmerControl(optimizer="bobyqa",
+                                       optCtrl=list(maxfun=2e5)))
+
+
+summary(model_0c)
+
+anova(model_0a, model_0c)
+
+
+###
+
+
+model_1a <- glmer(button_pressed ~ cond*negation*group + (1| subjectId), 
+                  family = "binomial", 
+                  data =  subset(exp, critical == "critical"),
+                  control=glmerControl(optimizer="bobyqa",
+                                       optCtrl=list(maxfun=2e5)))
+
+
+summary(model_1a)
+
+model_1b <- glmer(button_pressed ~ cond+negation+group + (1| subjectId), 
+                  family = "binomial", 
+                  data =  subset(exp, critical == "critical"),
+                  control=glmerControl(optimizer="bobyqa",
+                                       optCtrl=list(maxfun=2e5)))
+
+
+summary(model_1b)
 
 
 
-exp_qua %>%
-	separate(condition, into=c("quantifier", "t_condition"), sep="_", fill="right", extra="merge")	 %>%
-	ddply(.(quantifier, group, t_condition, subjectId), summarize, M = mean(button_pressed, na.rm =TRUE)) %>%
-	ddply(.(quantifier, group, t_condition), summarize, 
-		n    = length(!is.na(M)),
-		m = mean(M, na.rm=TRUE),
-        sd   = sd(M, na.rm=TRUE),
-        se   = sd / sqrt(n)
-		) %>%
-	ggplot(data=., aes(x=group, y=m)) +
-		geom_bar(stat="identity", position=position_dodge()) +
-		geom_errorbar(aes(ymin=m-se, ymax=m+se), width=.2,
-			position=position_dodge(.9))+
-		facet_grid(quantifier~t_condition) +
-		labs(title="", x="", y = "")
+anova(model_1a, model_1b)
+
+texreg(list(model_1a, model_1b), use.packages = FALSE, label = "Table 1", caption = "Two linear models.",
+       float.pos = "h")
+
+contrasts_1a <- emmeans(model_1a, ~cond*negation*group)
+sum <- summary(contrast(contrasts_1a, "tukey", simple = "each", combine = TRUE, adjust="none"))
+
+
+
+## hypothesis 1
+
+
+model_2a <- glmer(button_pressed ~ cond*negation + (1| subjectId), 
+                  family = "binomial", 
+                  data =  subset(exp, group == "no training" & critical == "critical"),
+                  control=glmerControl(optimizer="bobyqa",
+                                       optCtrl=list(maxfun=2e5)))
+
+
+summary(model_2a)
+
+
+model_2b <- glmer(button_pressed ~ negation+cond + (1| subjectId), 
+                  family = "binomial", 
+                  data =  subset(exp, group == "no training" & critical == "critical"),
+                  control=glmerControl(optimizer="bobyqa",
+                                       optCtrl=list(maxfun=2e5)))
+
+
+summary(model_2b)
+
+anova(model_2a, model_2b)
+
+
+texreg(list(model_2a, model_2b), use.packages = FALSE, label = "Table 1", caption = "Two linear models.",
+       float.pos = "h")
+
+contrasts_2a <- emmeans(model_2a, ~negation*cond)
+summary(contrast(contrasts_2a, "tukey", simple = "each", combine = TRUE, adjust="none"))
+
+
+model_3a <- glmer(button_pressed ~ group*negation + (1| subjectId), 
+                  family = "binomial", 
+                  data =  subset(exp, cond == "not upward-from red" & critical == "critical"),
+                  control=glmerControl(optimizer="bobyqa",
+                                       optCtrl=list(maxfun=2e5)))
+
+
+summary(model_3a)
+
+
+model_3b <- glmer(button_pressed ~ negation+group + (1| subjectId), 
+                  family = "binomial", 
+                  data =  subset(exp, cond == "not upward-from red" & critical == "critical"),
+                  control=glmerControl(optimizer="bobyqa",
+                                       optCtrl=list(maxfun=2e5)))
+
+
+summary(model_3b)
+
+anova(model_3a, model_3b)
+
+
+model_4a <- glmer(button_pressed ~ group*negation + (1| subjectId), 
+                  family = "binomial", 
+                  data =  subset(exp, cond == "upward-not from red" & critical == "critical"),
+                  control=glmerControl(optimizer="bobyqa",
+                                       optCtrl=list(maxfun=2e5)))
+
+
+summary(model_4a)
+
+
+model_4b <- glmer(button_pressed ~ negation+group + (1| subjectId), 
+                  family = "binomial", 
+                  data =  subset(exp, cond == "upward-not from red" & critical == "critical"),
+                  control=glmerControl(optimizer="bobyqa",
+                                       optCtrl=list(maxfun=2e5)))
+
+
+summary(model_4b)
+
+anova(model_4a, model_4b)
+
+
+contrasts_4a <- emmeans(model_4a, ~negation*group)
+summary(contrast(contrasts_4a, "tukey", simple = "each", combine = TRUE, adjust="none"))
+
+
+
+
